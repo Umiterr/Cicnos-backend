bd421390d35264b752959df399281bdf57b7aaaa
Umiterr <37944964+Umiterr@users.noreply.github.com>
2024-03-06 02:42:54 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// creating an Express instance
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// initializing a basic API that
+// returns the "Hello, World!" message
+app.get("/", (req, res) => {
+  res.json("Hello, World!");
+});
+
+// running the server
+app.listen(PORT, () => {
+  console.log(`Starting Express server on http://localhost:${PORT}`);
+});
